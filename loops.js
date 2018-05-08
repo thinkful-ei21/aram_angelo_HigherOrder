d442104c727fbaa65c63a5ce2d0e409780087d47
chagoy <angelochagoy@gmail.com>
2018-05-08 11:49:37 -0700

    turtle steps

--- loops.js
+++ loops.js
@@ -69,7 +69,7 @@
 // Return only names that begin with 'R'
 
 
-const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+//const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
 // const filteredNames = filter(myNames, function (name) {
 //   // This is a "predicate function" - it's a function that only returns a boolean
@@ -80,18 +80,55 @@ const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 // <---- DO NOT EDIT BETWEEN THESE LINES
 
 // TASK: DEFINE YOUR FILTER FUNCTION BELOW:
-function filter(arr, fn) {
-  let newArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (fn(arr[i])) {
-      newArray.push(arr[i]);
-    }
-  }
-  console.log(newArray);
+// function filter(arr, fn) {
+//   let newArray = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (fn(arr[i])) {
+//       newArray.push(arr[i]);
+//     }
+//   }
+//   console.log(newArray);
+// }
+
+//function startsWithR(str) {
+//  return str[0] === 'R';
+//}
+
+//filter(myNames, startsWithR);
+
+// function hazardWarningCreator(typeOfWarning) {
+//   let warningCounter = 0;
+
+//   return (location) => {
+//     warningCounter++;
+//     let times = warningCounter > 1 ? 'times' : 'time';
+//     console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`)
+//     console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} ${times} today!`)
+//   }
+// }
+
+// const rocksWarning = hazardWarningCreator('Rocks on the Road');
+// const alienWarning = hazardWarningCreator('Alien Invaders');
+// const shrekWarning = hazardWarningCreator('Shrek is loose');
+
+// rocksWarning('Main St and Pacific Ave');
+// rocksWarning('Centinela Ave and Olympic Blvd');
+
+// alienWarning('New York City');
+
+// alienWarning("Magical Swamp");
+// alienWarning("Magical Swamp");
+// alienWarning("Magical Swamp");
+// alienWarning("Magical Swamp");
+
+function removeNegative(arr) {
+  let count = 0;
+
+  arr.filter((steps) => steps[0] >= 0 && steps[1] >= 0)
+    .map((negativeSteps) => negativeSteps[0] + negativeSteps[1])
+    .forEach((sum) => count += sum);
+
+  return count;
 }
 
-function startsWithR(str) {
-  return str[0] === 'R';
-}
-
-filter(myNames, startsWithR);
+console.log(removeNegative([[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]]))
