f44da08da386fe9b092835fad6d4cc6b4c7816e3
chagoy <angelochagoy@gmail.com>
2018-05-08 09:13:51 -0700

    average

--- loops.js
+++ loops.js
@@ -1,38 +1,48 @@
 'use strict';
-const numbers = [10, 4, 6, 31243, 532, 534, 0, 1, 2, 3];
 
-console.log(numbers.length);
-
-function max(arr) {
-
-  let max = 0;
-  let i = 0;
-  while (i < arr.length) {
-    if (arr[i] > max) {
-      max = arr[i];
-
-    }
-    i++;
-  }
-  console.log(max);
-}
+// function average(arr) {
+//   let sum = 0;
+//   arr.forEach( (a) => {
+//     sum += a
+//   });
+//   console.log(sum / arr.length);
+// }
 
 
+// const numbers = [7, 8, 9];
+// average(numbers);
+// console.log(numbers.length);
 
-function min(arr) {
-  // your code here
-  let min = arr[0];
-  let i = 1;
+// function max(arr) {
 
-  while (i < arr.length) {
-    if (arr[i] < min) {
-      min = arr[i];
+//   let max = 0;
+//   let i = 0;
+//   while (i < arr.length) {
+//     if (arr[i] > max) {
+//       max = arr[i];
 
-    }
-    i++;
-  }
-  console.log(min);
-}
+//     }
+//     i++;
+//   }
+//   console.log(max);
+// }
 
-max(numbers);
-min(numbers);
+
+
+// function min(arr) {
+//   // your code here
+//   let min = arr[0];
+//   let i = 1;
+
+//   while (i < arr.length) {
+//     if (arr[i] < min) {
+//       min = arr[i];
+
+//     }
+//     i++;
+//   }
+//   console.log(min);
+// }
+
+// max(numbers);
+// min(numbers);
