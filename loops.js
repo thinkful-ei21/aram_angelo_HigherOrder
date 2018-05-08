8d7a422349e8958df68495dd46e15755d7994c86
chagoy <angelochagoy@gmail.com>
2018-05-08 11:17:46 -0700

    filter r

--- loops.js
+++ loops.js
@@ -48,19 +48,50 @@
 // min(numbers);
 
 
-function repeat(fn, n) {
-  for (let i = 0; i < n; i++) {
-    fn();
+// function repeat(fn, n) {
+//   for (let i = 0; i < n; i++) {
+//     fn();
+//   }
+// }
+
+// function hello() {
+//   console.log('Hello world');
+// }
+
+// function goodbye() {
+//   console.log('Goodbye world');
+// }
+
+// repeat(hello, 5);
+// repeat(goodbye, 5);
+
+// DO NOT EDIT BETWEEN THESE LINES ----->
+// Return only names that begin with 'R'
+
+
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// const filteredNames = filter(myNames, function (name) {
+//   // This is a "predicate function" - it's a function that only returns a boolean
+//   return name[0] === 'R';
+// });
+
+// console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i])) {
+      newArray.push(arr[i]);
+    }
   }
+  console.log(newArray);
 }
 
-function hello() {
-  console.log('Hello world');
+function startsWithR(str) {
+  return str[0] === 'R';
 }
 
-function goodbye() {
-  console.log('Goodbye world');
-}
-
-repeat(hello, 5);
-repeat(goodbye, 5);
+filter(myNames, startsWithR);
